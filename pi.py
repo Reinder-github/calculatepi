965a67b651bb7567b0f6dc24fbf40f7935832b82
Reinder-github <71931698+Reinder-github@users.noreply.github.com>
2021-04-17 17:12:36 +0200

    upate
    Fixed the close button, which didn't work when test() was running,

--- pi.py
+++ pi.py
@@ -2,11 +2,18 @@ import random
 import math
 import pygame
 
+def stop():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.display.quit()
+            pygame.quit()
+
 def test(aantal):
     aantalbinnencirkel = 0
     aantaltotaal = 0
     global screen
     for x in range (aantal):
+        stop()
         xrand1 = random.uniform(0,500)
         yrand2 = random.uniform(0,500)
         afstand = math.sqrt((xrand1-250)**2+(yrand2-250)**2)
@@ -27,8 +34,7 @@ screen.fill((255,255,255))
 pygame.draw.circle(screen, (255,0,0), (250,250), (250), (1))
 pygame.display.flip()
 
-
-resultaat = test(10000)
+resultaat = test(10000000)
 font = pygame.font.Font('freesansbold.ttf', 32)  
 text = font.render('Pi is ' + str(resultaat), True, (255,255,255), (145,135,135)) 
 textRect = text.get_rect()  
